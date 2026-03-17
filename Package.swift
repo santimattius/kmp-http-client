a714c08bc252102ea0c318f0a40720f7c948df5c
GitHub Action <action@github.com>
2026-03-17 23:09:39 +0000

    Update Package.swift for version 1.1.0-ALPHA01

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KMPHttpClient",
-            url: "https://github.com/santimattius/kmp-http-client/releases/download/v1.0.0/KMPHttpClient-1.0.0.xcframework.zip",
-            checksum: "14d876ae46da61285c56df892130d92c7c2ec4177bb1cc5a584ea82f0372f57d"
+            url: "https://github.com/santimattius/kmp-http-client/releases/download/1.1.0-ALPHA01/KMPHttpClient-1.1.0-ALPHA01.xcframework.zip",
+            checksum: "fd1f1163cd91b56e4cf1c300ab4a21d298fb3a65a56101a7975926f51a1ab4ae"
         )
     ]
 )
